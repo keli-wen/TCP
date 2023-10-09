552632b2d2dd30e4082912361ea271b6dea2bbef
keli-wen <whuwkl@gmail.com>
2023-10-09 16:56:07 +0000

    Lab2 First Draft (need more concise comments for these codes)

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -11,9 +11,42 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    const auto &header = seg.header();
+
+    if (!_isn.has_value()) {
+        // Drop before *LISTEN*.
+        if (!header.syn) return void();
+        // Set the Initial Seq Number.
+        _isn = header.seqno;
+    }
+
+    // *SYN RECV*
+    //? Why we need this?
+    // WrappingInt32 seqno = header.seqno;
+
+    // Denote the first byte that the receiver needs from the sender.
+    uint64_t abs_ackno = stream_out().bytes_written() + 1;
+    uint64_t abs_seqno = unwrap(header.seqno, _isn.value(), abs_ackno);
+
+    //! Note: Need more explanation?
+    uint64_t stream_index = abs_seqno + header.syn - 1;
+
+    // If header.fin is true, then state tranfer to *FIN_RECV*.
+    _reassembler.push_substring(
+        seg.payload().copy(), stream_index, header.fin);
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const {
+    // LISTEN
+    if (!_isn.has_value()) return nullopt;
+    // SYN_RECV
+    WrappingInt32 ackno = wrap(stream_out().bytes_written() + 1, _isn.value());
+    // FIN_RECV
+    if (stream_out().input_ended()) ackno = ackno + 1;
+    return ackno;
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const {
+    // Window size = capacity - buffer already received.
+    return _capacity - stream_out().buffer_size();
+}
