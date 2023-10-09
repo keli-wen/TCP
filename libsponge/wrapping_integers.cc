e6c36a0acc929746855f60736151da0a45910a39
keli-wen <whuwkl@gmail.com>
2023-10-09 14:58:20 +0000

    Implement the WrappingInt32 Class

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -14,8 +14,7 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    return WrappingInt32(static_cast<uint32_t>(n + isn.raw_value()));
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +28,21 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    // Since the low 32 bits don't influence the result, we can directly
+    // calculate them.
+    uint32_t low32bit = static_cast<uint32_t>(n - isn);
+    uint64_t hight32bit_add1 = (checkpoint + (1 << 31)) & 0xFFFFFFFF00000000;
+    uint64_t hight32bit_minus1 = (checkpoint - (1 << 31)) & 0xFFFFFFFF00000000;
+    uint64_t absolute_add1 = hight32bit_add1 | low32bit;
+    uint64_t absolute_minus1 = hight32bit_minus1 | low32bit;
+
+    // Compare the distance between the checkpoint and the two possible
+    // results.
+    if (max(absolute_add1, checkpoint) - min(absolute_add1, checkpoint) <=
+        max(absolute_minus1, checkpoint) - min(absolute_minus1, checkpoint)) {
+        return absolute_add1;
+    } else {
+        return absolute_minus1;
+    }
+    return -1; // Error.
 }
