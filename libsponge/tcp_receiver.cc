34db2b2e22851814c029ef57f59e51da90b32614
keli-wen <whuwkl@gmail.com>
2023-10-11 11:03:09 +0000

    ✅ Lab2, Add more explanation at the confusing code

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -21,14 +21,18 @@ void TCPReceiver::segment_received(const TCPSegment &seg) {
     }
 
     // *SYN RECV*
-    //? Why we need this?
-    // WrappingInt32 seqno = header.seqno;
-
-    // Denote the first byte that the receiver needs from the sender.
+    // The define of ackno: The sequence number of the next byte expected on
+    // the stream. Obviously, the stream_out().bytes_written() is the number
+    // of bytes already received. So we need to plus 1.
     uint64_t abs_ackno = stream_out().bytes_written() + 1;
     uint64_t abs_seqno = unwrap(header.seqno, _isn.value(), abs_ackno);
 
-    //! Note: Need more explanation?
+    //! Note: Need more explanation? why plus 1?
+    //! Answer: First, we should know that stream_index should not count the
+    //! SYN and FIN. So usually, the stream_index should be seqno - 1. (We can
+    //! note that abs_ackno is the seqno of the SYN, so actually, the abs_seqno
+    //! of the first byte should be abs_ackno + 1 and the stream_index is
+    //! abs_seqno), but if the SYN is false, we need to minus 1.
     uint64_t stream_index = abs_seqno + header.syn - 1;
 
     // If header.fin is true, then state tranfer to *FIN_RECV*.
